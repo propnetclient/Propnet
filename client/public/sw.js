ae54e79bd0abe3f76022219943d67c9a94f5626c
KPgdm <40068142-KPgdm@users.noreply.replit.com>
2025-06-21 11:55:10 +0000

    Restored to 'd441f2cb9157c396b029e3915b0b28cb4cc30381'
    Replit-Restored-To: d441f2cb9157c396b029e3915b0b28cb4cc30381

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,16 +1,9 @@
-const CACHE_NAME = 'propnet-v4';
+const CACHE_NAME = 'propnet-v3';
 const urlsToCache = [
   '/',
   '/manifest.json',
   '/icon-propnet.svg',
-  '/icon-propnet-new.svg',
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
   '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
   '/icons/icon-512x512.png'
 ];
 
